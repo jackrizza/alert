7aab4659152fbfadf47f81c5d781d56cc48c3176
jackrizza <jackrizza@gmail.com>
2015-04-19 21:19:13 -0400

    Updated the nodejs server

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use("/css", express.static(__dirname + '/css'));
+
+app.use("/js", express.static(__dirname + '/js'));
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));

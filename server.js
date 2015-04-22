b5bd17d38ee894011c0c524bd3bac3f3ffb01785
Jack Rizza <jackrizza@gmail.com>
2015-04-22 14:54:15 -0400

    Updated index.html and server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/index.html',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/index', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 app.listen(3000);
 
 console.log("Running at Port 3000");
